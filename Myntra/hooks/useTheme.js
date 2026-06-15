2b04e0b7e328eeaca0d9d7f96d7c6e4a3fff250b
Kunal <kunal2jaykare@gmail.com>
2026-06-15 14:22:37 +0530

    first commit

--- Myntra/hooks/useTheme.js
+++ Myntra/hooks/useTheme.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "@/theme/ThemeContext";
+import { ThemeContext } from "/constants/context/ThemeContext.tsx";
 
 export const useTheme = () => {
     const context = useContext(ThemeContext);
